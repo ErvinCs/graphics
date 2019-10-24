c8bb51ced34277fff7ea8b136bba4fc87b43b7b6
ErvinCs <csoka.ervin12@gmail.com>
2019-10-24 14:19:01 +0300

    Bad Sin, Segment, Click-to-Point

---
+++ GLab1/GLab1/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLab1
+{
+    class Line
+    {
+        private Point a;
+        private Point b;
+
+        public Point A { get => a; set => a = value; }
+        public Point B { get => b; set => b = value; }
+
+        public Line(Point a, Point b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public float Slope()
+        {
+            return (a.Y - b.Y) / (a.X - b.X);
+        }
+    }
+}
---
+++ GLab1/GLab1/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLab1
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return "Point(X=" + this.x + ", Y=" + this.y + ')';
+        }
+    }
+}
