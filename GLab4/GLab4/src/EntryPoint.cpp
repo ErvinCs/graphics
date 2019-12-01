1b04d2cac4791ff2ba61c997bb2e9e845d420e9b
ErvinCs <csoka.ervin12@gmail.com>
2019-12-01 22:22:39 +0200

    Horizontally Moving Sqaure

--- GLab4/GLab4/src/EntryPoint.cpp
+++ GLab4/GLab4/src/EntryPoint.cpp
@@ -2,23 +2,27 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+float xPos = -10;
+bool xDirRight = true;
+const float xStep = 0.2;
+
 void displayCallback()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();	// Reset the parameters of the model-view matrix 
 
 	// Draw calls
-	glPointSize(10);
-	glBegin(GL_TRIANGLES);
+	glBegin(GL_POLYGON);
 	
-	// Specify Vertices
-	glVertex2f(0.0f, 5.0f);
-	glVertex2f(-4.0f, -3.0f);
-	glVertex2f(4.0f, -3.0f);
+	// Specify Vertices - anti-clockwise fashion
+	glVertex2f(xPos,	   1.0f);
+	glVertex2f(xPos,	  -1.0f);
+	glVertex2f(xPos+2.0f, -1.0f);
+	glVertex2f(xPos+2.0f,  1.0f);
 
 	glEnd();
 
-	glFlush();
+	glutSwapBuffers();
 }
 
 // Called on window reshape
@@ -35,10 +39,38 @@ void reshapeCallback(int width, int height)
 	glMatrixMode(GL_MODELVIEW); 
 }
 
+void timer(int)
+{
+	// Also call Display 60 times in 1 second - OpenGL does it by itself
+	glutPostRedisplay();
+	// Timer calls itself periodically - 60fps
+	glutTimerFunc(1000 / 60, timer, 0);
+
+	// BUGGED HERE
+	// Update X
+	switch (xDirRight)
+	{
+	case true:
+		if (xPos < 8)
+			xPos += xStep;
+		else
+			xDirRight = false;
+		break;
+	case false:
+		if (xPos > -10)
+			xPos -= xStep;
+		else
+			xDirRight = true;
+		break;
+	}
+	std::cout << "Timer-Step: xPos=" << xPos << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
+	// RGB & Double-Buffer display mode
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
 	glutInitWindowPosition(200, 100);
 	glutInitWindowSize(500, 500);
@@ -47,10 +79,10 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(displayCallback);
 	glutReshapeFunc(reshapeCallback);
-	
+	// Call a function after a specified ammount of time
+	glutTimerFunc(1000, timer, 0);
+
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	glutMainLoop();
-
-	return 0;
 }
