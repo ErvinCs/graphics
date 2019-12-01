8c78200005bbe9bfa9d95afff3eac561319675e2
ErvinCs <csoka.ervin12@gmail.com>
2019-12-01 16:23:43 +0200

    Just a triangle

--- GLab4/GLab4/src/EntryPoint.cpp
+++ GLab4/GLab4/src/EntryPoint.cpp
@@ -2,8 +2,38 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-void displayCallback();
+void displayCallback()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();	// Reset the parameters of the model-view matrix 
 
+	// Draw calls
+	glPointSize(10);
+	glBegin(GL_TRIANGLES);
+	
+	// Specify Vertices
+	glVertex2f(0.0f, 5.0f);
+	glVertex2f(-4.0f, -3.0f);
+	glVertex2f(4.0f, -3.0f);
+
+	glEnd();
+
+	glFlush();
+}
+
+// Called on window reshape
+void reshapeCallback(int width, int height)
+{
+	// Set Viewport
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	// Set Projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();	// Reset the parameters of the projection matrix 
+
+	gluOrtho2D(-10, 10, -10, 10);
+	glMatrixMode(GL_MODELVIEW); 
+}
 
 int main(int argc, char** argv)
 {
@@ -16,19 +46,11 @@ int main(int argc, char** argv)
 	glutCreateWindow("Test");
 
 	glutDisplayFunc(displayCallback);
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glutReshapeFunc(reshapeCallback);
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	glutMainLoop();
 
 	return 0;
-}
-
-void displayCallback()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glLoadIdentity();
-
-	// Draw calls here
-
-	glFlush();
 }
